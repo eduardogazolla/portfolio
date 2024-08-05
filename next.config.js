237073a68706d1170b55552e018d0484722f977d
Eduardo I. Gazolla <eduardo.gazolla@edu.unipar.br>
2024-08-05 10:34:26 -0300

    projeto finalizado

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["sa-east-1.graphassets.com"],
+  },
+};
+
+module.exports = nextConfig;
